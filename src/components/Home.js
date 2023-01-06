a3de24d34b40d7867ed43c834225936d8e9701d8
Aric Essig <aric.essig@pinnacol.com>
2023-01-06 17:22:25 -0600

    embed working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,38 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import ReactPlayer from "react-player";
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, Stack } from "@mui/material";
 
 export const Home = () => {
+  const [value, setValue] = useState("")
+  const [url, setUrl] = useState("https://www.youtube.com/watch?v=fy-bd5AC-Ms&ab_channel=LanguageofEarth")
   const fileInput = useRef(null);
-  const handleClick = (e) => {
-   //  e.preventDefault();
+  // const handleClick = (e) => {
+  //   console.log();
+    
    
-    console.log(fileInput.current.value);
-  };
+  //   console.log(fileInput.current.value);
+  // };
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(value);
+   setUrl(value) 
+  }
   return (
-    <>
-      <ReactPlayer url="https://www.youtube.com/watch?v=fy-bd5AC-Ms&ab_channel=LanguageofEarth" />
-      <form >
-        <TextField id="input" type="text" ref={fileInput}>
+    <Stack spacing={2} padding={4}>
+      <ReactPlayer url={url} />
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={2}>
+        <TextField id="input" type="text" ref={fileInput} value={value} onChange={handleChange} sx={{width: '100%'}}>
           asdf
         </TextField>
-        <Button variant="outlined" onClick={handleClick}>
+        <Button type="submit" variant="contained" sx={{ width: "400px"}} >
           Enter a URL
         </Button>
+        </Stack>
       </form>
-    </>
+    </Stack>
   );
 };
